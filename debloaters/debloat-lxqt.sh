957b093c1f6d11dd3d466b5e4425cdfa896da9e8
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-07-19 12:58:35 +0300

    debloaters: add a few more packages to lxqt
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- debloaters/debloat-lxqt.sh
+++ debloaters/debloat-lxqt.sh
@@ -5,4 +5,4 @@ blinken cantor cantor-dev kbruch plasma-marble kgeography khangman klettres ktou
 minuet parley rocs step kdegames palapeli marble gwenview kcolorchooser kruler kolourpaint kolourpaint4 lximage-qt okular screengrab libreoffice \
 kmail kaddressbook korganizer ktnef kontact lokalize pim-sieve-editor qpdfview akonadiconsole cervisia kapptemplate kcachegrind kimagemapeditor kuiviewer \
 umbrello audacious dragonplayer juk rhythmbox smplayer smtube timidity bleachbit kwalletmanager qps tilix kmag kmousetool kmouth warpinator \
-sweeper kget firefox konqueror kopete krfb meteo-qt pim-data-exporter quassel uget
+sweeper kget firefox konqueror kopete krfb meteo-qt pim-data-exporter quassel uget akregator krdc libreoffice-common
