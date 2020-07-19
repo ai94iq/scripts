ca047e13012ac7ed5345e909f0fcae854c97482c
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-07-19 12:49:37 +0300

    debloaters: add mint and lxqt
    test on mint cinnamon and lxqt-desktop
    
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

---
+++ debloaters/debloat-lxqt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt autoremove --purge ark feathernotes featherpad kalarm kcalc kcharselect knotes kteatime ktimer kwrite kleopatra okteta okteta-dev onboard qlipper artikulate \
+blinken cantor cantor-dev kbruch plasma-marble kgeography khangman klettres ktouch kturtle kwordquiz kalzium kanagram kig kiten kmplot \
+minuet parley rocs step kdegames palapeli marble gwenview kcolorchooser kruler kolourpaint kolourpaint4 lximage-qt okular screengrab libreoffice \
+kmail kaddressbook korganizer ktnef kontact lokalize pim-sieve-editor qpdfview akonadiconsole cervisia kapptemplate kcachegrind kimagemapeditor kuiviewer \
+umbrello audacious dragonplayer juk rhythmbox smplayer smtube timidity bleachbit kwalletmanager qps tilix kmag kmousetool kmouth warpinator \
+sweeper kget firefox konqueror kopete krfb meteo-qt pim-data-exporter quassel uget
---
+++ debloaters/debloat-mint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt autoremove --purge pix hexchat thunderbird transmission libreoffice \
+gnome-orca gnote graphviz hexchat xed xreader xviewer celluloid rhythmbox \
+redshift drawing simple-scan transmission libreoffice gnome-calendar
