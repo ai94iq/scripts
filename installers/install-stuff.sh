ec116630fa251f69ac72a9d121d4d37ae8ec2e68
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-08-11 14:55:49 +0300

    fix OC'D
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- installers/install-stuff.sh
+++ installers/install-stuff.sh
@@ -6,20 +6,20 @@ if [ $? = 0 ]; then
 			#Add Keys/Repo's#
 
 			#Sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+			wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+			sudo apt-get install apt-transport-https
+			echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 			#Plata
-sudo add-apt-repository ppa:tista/plata-theme
+			sudo add-apt-repository ppa:tista/plata-theme
 
 			#IconPacks
-sudo add-apt-repository ppa:papirus/papirus
+			sudo add-apt-repository ppa:papirus/papirus
 
 			#Wine
-sudo dpkg --add-architecture i386
-wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+			sudo dpkg --add-architecture i386
+			wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+			sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
 
 			#Etc.
 
