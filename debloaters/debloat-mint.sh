818b619b58945a84bb50e959c6b8b6fd048532ff
anime-is-real <aweldon2701@gmail.com>
2020-08-11 14:05:13 +0300

    add mint debloater from Aaron-dev
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- debloaters/debloat-mint.sh
+++ debloaters/debloat-mint.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
 
-sudo apt autoremove --purge pix hexchat thunderbird transmission libreoffice \
-gnome-orca gnote graphviz hexchat xed xreader xviewer celluloid rhythmbox \
-redshift drawing simple-scan transmission libreoffice gnome-calendar
+zenity --question --text="Do you want to debloat your system?" --no-wrap
+if [ $? = 0 ]; then
+
+
+	## Remove the bloat
+	sudo apt purge redshift -y
+	sudo apt purge libreoffice-core -y
+	sudo apt purge libreoffice-common -y
+	sudo apt purge transmission-gtk -y
+	sudo apt purge hexchat -y
+	sudo apt purge baobab -y
+	sudo apt purge seahorse -y
+	sudo apt purge thunderbird -y
+	sudo apt purge rhythmbox -y
+	sudo apt purge pix -y
+	sudo apt purge simple-scan -y
+	sudo apt purge drawing -y
+	sudo apt purge xed -y
+	sudo apt purge gnote -y
+	sudo apt purge xreader -y
+	sudo apt purge onboard -y
+	sudo apt purge gnome-calendar -y
+	sudo apt purge gnome-calculator -y
+	sudo apt purge celluloid -y
+	sudo apt purge gnome-logs -y
+	sudo apt purge gnome-power-manager -y
+	sudo apt purge onboard -y
+
+	sudo apt autoremove -y && sudo apt clean
+else
+	exit
+fi
+
+zenity --question --text "Do you want flatpak and to mark it on hold?" --no-wrap
+if [ $? = 0 ]; then
+
+	##mark packages on hold
+	sudo apt purge flatpak
+	sudo apt-mark hold flatpak
+else
+	exit
+fi
+
+
+zenity --question --text "Would you like to update your system?" --no-wrap
+if [ $? = 0 ]; then
+
+	#update and upgrade mint
+	sudo apt update && sudo apt upgrade -y
+
+else
+	exit
+fi
+
+exit
