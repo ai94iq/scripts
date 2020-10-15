daaa4ad29bf0e79b96db942229c57617ddc0e84a
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-10-15 13:59:15 +0300

    add fedora stuff

---
+++ installers/install-stuff-fedora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#enable-rpmfusion
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+#Sublime-text
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/dev/x86_64/sublime-text.repo
+
+#plata-theme
+sudo dnf copr enable tonyjames/plata-theme
+
+#install-everything
+sudo dnf install sublime-text papirus-icon-theme vlc google-roboto-fonts.noarch meld gparted telegram-desktop chromium htop neofetch plata-theme numix-icon-theme arc-theme
