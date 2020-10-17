8937cdfa3bfaaf51f466499d7878c67ddff49e34
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-10-17 21:12:18 +0300

    add arch based installer
    yay has to installed

---
+++ installers/install-stuff-arch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Sublime-text
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+
+sudo pacman -Syu ; sudo pacman -Syu sublime-text thermald lm_sensors psensor figlet-mode fortune
+
+#install-stuff
+yay -Syu flatpak papirus-icon-theme vlc meld gparted telegram-desktop chromium htop neofetch plata-theme numix-circle-arc-icons-git numix-circle-icon-theme-git numix-cursor-theme-git ccache-git
