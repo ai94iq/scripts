4c910448f29f146525abbe4c1aa28dca0b0c9e74
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-01-24 12:10:05 +0300

    installers: add Spotify flatpak to Arch's script
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- installers/install-stuff-arch.sh
+++ installers/install-stuff-arch.sh
@@ -10,3 +10,6 @@ sudo pacman -Syu ; sudo pacman -Syu sublime-text thermald lm_sensors psensor ncd
 #install-stuff
 yay -Syu flatpak papirus-icon-theme vlc meld gparted telegram-desktop chromium htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear libcurl3-gnutls libcurl-openssl-1.0 aria2 aria2c-daemon
 
+
+#flatpaks
+flatpak install flathub com.spotify.Client
