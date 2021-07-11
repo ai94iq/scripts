45c260c84d3cbb1c02d447c95e53d5177280c480
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-07-11 07:22:01 +0300

    installers: update arch
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- installers/install-stuff-arch.sh
+++ installers/install-stuff-arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #install-stuff
-yay -Syu papirus-icon-theme vlc meld gparted telegram-desktop google-chrome gdu sublime-text-dev htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear gtk-engine-murrine ttf-roboto marco
+yay -Syu papirus-icon-theme vlc meld gparted telegram-desktop google-chrome gdu appimagelauncher appstream appimage-manager appimage-thumbnailer-git appimage-appimage ocs-url vscode htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear gtk-engine-murrine ttf-roboto marco
