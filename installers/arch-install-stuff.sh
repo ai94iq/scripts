2d1db95eea92e8a17246f53c11519d79595f1bc6
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-07-30 16:53:55 +0300

    installers: arch: add back sublime text
    remove vscode also
    
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- installers/arch-install-stuff.sh
+++ installers/arch-install-stuff.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#sublime-text
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu sublime-text
+
+
 #install-stuff
-yay -Syu papirus-icon-theme vlc meld gparted telegram-desktop google-chrome gdu ttf-ms-fonts noto-fonts-emoji appimagelauncher appstream appimage-manager appimage-thumbnailer-git appimage-appimage ocs-url vscode htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear gtk-engine-murrine ttf-roboto marco
+yay -Syu papirus-icon-theme vlc meld gparted telegram-desktop google-chrome gdu ttf-ms-fonts noto-fonts-emoji appimagelauncher appstream appimage-manager appimage-thumbnailer-git appimage-appimage ocs-url htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear gtk-engine-murrine ttf-roboto marco
