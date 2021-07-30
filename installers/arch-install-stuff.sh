afd800e7ccb9149eb792fe9b60ad12d96dda09eb
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-07-30 17:01:51 +0300

    installers: arch: add some packages
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- installers/arch-install-stuff.sh
+++ installers/arch-install-stuff.sh
@@ -7,4 +7,4 @@ sudo pacman -Syu sublime-text
 
 
 #install-stuff
-yay -Syu papirus-icon-theme vlc meld gparted telegram-desktop google-chrome gdu ttf-ms-fonts noto-fonts-emoji appimagelauncher appstream appimage-manager appimage-thumbnailer-git appimage-appimage ocs-url htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear gtk-engine-murrine ttf-roboto marco
+yay -Syu papirus-icon-theme meld gparted telegram-desktop google-chrome gdu ttf-ms-fonts noto-fonts-emoji appimagelauncher appstream appimage-manager appimage-thumbnailer-git appimage-appimage ocs-url htop neofetch plata-theme thermald lm_sensors psensor mate-terminal ttf-opensans drun intel-ucode-clear gtk-engine-murrine ttf-roboto marco flameshot spotify octopi-dev celluloid youtube-dl
