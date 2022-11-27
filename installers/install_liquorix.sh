a3102b68cef7afb0dcf1bda719e13ad05a8f80f6
Abdulwahab Isam <abdoi94.iq@gmail.com>
2022-11-27 11:47:49 +0300

    installers: remove some scripts
    I've never used them, so meh

--- installers/install_liquorix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo add-apt-repository ppa:damentz/liquorix && sudo apt-get update
-
-sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
--- installers/install_nvidia.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo add-apt-repository ppa:graphics-drivers && sudo apt-get update
-
-sudo apt-get install nvidia-driver-450-server
-
