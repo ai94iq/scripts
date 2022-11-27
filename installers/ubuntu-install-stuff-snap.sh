5732bb0899bc5433a17b0cf1514d146ae5700775
Abdulwahab Isam <abdoi94.iq@gmail.com>
2022-11-27 11:44:59 +0300

    installers: update and rename Ubuntu script
    use snap for some packages also!

---
+++ installers/ubuntu-install-stuff-snap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+			#Add Keys/Repo's
+			#Installing
+			sudo apt update
+			sudo apt-get install psensor fonts-roboto ttf-mscorefonts-installer telegram-desktop htop neofetch
+			#Installing Snaps
+			sudo snap install sublime-text	--classic
+			sudo snap install vlc	--classic
+			sudo snap install code	--classic
+			sudo snap install spotify	--classic
+exit
