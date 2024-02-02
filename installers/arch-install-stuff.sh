a9970f979b649920b91ea9f9303be76bbab8f022
Abdulwahab Isam <abdoi94.iq@gmail.com>
2024-02-02 10:26:00 +0300

    Installers: Change Arch Script a bit
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- installers/arch-install-stuff.sh
+++ installers/arch-install-stuff.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#sublime-text
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-sudo pacman -Syu sublime-text
-
-
 #install-stuff
-yay -Sy papirus-icon-theme meld gparted telegram-desktop google-chrome gdu \
-		 ttf-ms-fonts noto-fonts-emoji appimagelauncher appstream appimage-manager \
-		 appimage-thumbnailer-git appimage-appimage ocs-url htop neofetch plata-theme \
-		 thermald lm_sensors psensor ttf-opensans intel-ucode-clear gtk-engine-murrine \
-		 ttf-roboto flameshot spotify celluloid youtube-dl rofi
+yay -Sy		meld gparted telegram-desktop google-chrome gdu \
+		 	ttf-ms-fonts noto-fonts-emoji \
+		 	htop neofetch ttf-opensans \
+		 	ttf-roboto flameshot spotify celluloid \
+		 	youtube-dl rofi visual-studio-code-bin \
+		 	bcompare discord_arch_electron \
+		 	github-desktop-bin mailspring-bin \
+		 	jdownloader2 oh-my-bash-git
